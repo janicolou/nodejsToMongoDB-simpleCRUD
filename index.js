aa8bbb758a43c3104cb4ce392fde6180eed3dee4
janicolou <gabo.janicolo@gmail.com>
2021-05-04 17:37:25 +0100

    added comments in api

--- index.js
+++ index.js
@@ -24,27 +24,32 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+// Setting the port to listen to
 app.listen(process.env.PORT, () => {
 	console.log(`Listening on port ${process.env.PORT}`);
 });
 
+// Getting all of the items in the form
 app.get("/", async (req, res) => {
 	const items = await Item.find({});
 	res.render("items/mainForm", { items });
 });
 
+// Posting a new item
 app.post("/items", async (req, res) => {
 	const newItem = new Item(req.body);
 	await newItem.save();
 	res.redirect(`/`);
 });
 
+// Viewing the edit tab and posting the preset value of the item
 app.post("/items/:id/edit", async (req, res) => {
 	const { id } = req.params;
 	const item = await Item.findById(id);
 	res.render("items/edit", { item });
 });
 
+// Putting the item into the database
 app.put("/items/:id", async (req, res) => {
 	const { id } = req.params;
 	const item = await Item.findByIdAndUpdate(id, req.body, {
@@ -54,6 +59,7 @@ app.put("/items/:id", async (req, res) => {
 	res.redirect(`/`);
 });
 
+// Delete Post Method
 app.delete("/items/:id", async (req, res) => {
 	const { id } = req.params;
 	const deletedItem = await Item.findByIdAndDelete(id);
